6866d6c8e0c701eee3a54476e741c2a2922d3363
sudoKush <404kushfound@gmail.com>
2025-09-04 15:25:08 +0530

    feat(nvim): add seamless navigation between neovim panes and tmux panes

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2,7 +2,27 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'   -- packer manages itself
-  use 'rebelot/kanagawa.nvim'
-  use 'christoomey/vim-tmux-navigator'
+
+  use 'rebelot/kanagawa.nvim' -- theme
+
+  use {
+    'christoomey/vim-tmux-navigator', -- for navigation between tmux panes
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  }
 end)
+
 
