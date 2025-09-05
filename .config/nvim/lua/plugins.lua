f926a962e2dbdd405bad06ec91cc73e692dc2852
sudoKush <404kushfound@gmail.com>
2025-09-05 08:00:00 +0530

    chore(nvim): enable auto-regeneration for packer_compiled.lua and fix plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,22 +1,23 @@
 vim.cmd [[packadd packer.nvim]]
 
--- for auto regeneration for packer_compile.lua file
+-- auto regenerate packer_compile.lua on save
 vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-  augroup end
+augroup packer_user_config
+autocmd!
+autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+augroup end
 ]]
 
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'   -- packer manages itself
+	use 'wbthomason/packer.nvim'   -- packer manages itself
+	use 'rebelot/kanagawa.nvim'    -- theme
+	use 'nvim-lua/plenary.nvim' -- dependency for telescope
+	use 'nvim-telescope/telescope.nvim' -- fuzzy finder
+	use {
+		'christoomey/vim-tmux-navigator', -- tmux navigation
+		config = function()
+			require('keymaps').setup()
+		end
+	}
+end)
 
-  use 'rebelot/kanagawa.nvim' -- theme
-
-  use {
-    'christoomey/vim-tmux-navigator', -- for navigation between tmux panes
-    config = function()
-      require('keymaps').setup()
-    end
-
-  } end)
