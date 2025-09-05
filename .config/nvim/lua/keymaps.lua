a27170145c94558cee6a4b4b97b6dc892e68b49c
sudoKush <404kushfound@gmail.com>
2025-09-05 08:00:00 +0530

    chore(nvim): update telescope keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,17 +1,19 @@
-vim.g.mapleader = " " -- space is the leader key
+local opts = { noremap = true, silent = true }
+vim.g.mapleader = " "
 
+local keymap = vim.keymap
+keymap.set("n", "<leader><leader>", "<cmd>Telescope find_files<cr>", { desc = "Find files in cwd" })
+keymap.set("n", "<leader>r", "<cmd>Telescope oldfiles<cr>", { desc = "Recently opened files" })
+keymap.set("n", "<leader>g", "<cmd>Telescope live_grep<cr>", { desc = "Grep in cwd" })
+keymap.set("n", "<leader>k", "<cmd>Telescope keymaps<cr>", { desc = "Search keymaps" })
 
--- tmux navigation between panes keymaps
 local M = {}
-
 function M.setup()
-  local opts = { noremap = true, silent = true }
   vim.api.nvim_set_keymap('n', '<C-h>', ':TmuxNavigateLeft<CR>', opts)
   vim.api.nvim_set_keymap('n', '<C-j>', ':TmuxNavigateDown<CR>', opts)
   vim.api.nvim_set_keymap('n', '<C-k>', ':TmuxNavigateUp<CR>', opts)
   vim.api.nvim_set_keymap('n', '<C-l>', ':TmuxNavigateRight<CR>', opts)
   vim.api.nvim_set_keymap('n', '<C-\\>', ':TmuxNavigatePrevious<CR>', opts)
 end
-
 return M
 
