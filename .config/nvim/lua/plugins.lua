7576c4af9095ebdd66efb45968ae0844ec8b0915
sudoKush <404kushfound@gmail.com>
2025-09-06 00:00:00 +0530

    feat(nvim): change the theme and add nvim treesitter for syntax highlighting

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -10,7 +10,16 @@ augroup end
 
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'   -- packer manages itself
-	use 'rebelot/kanagawa.nvim'    -- theme
+	use {
+      'sainnhe/gruvbox-material', -- theme
+      lazy = false,
+      priority = 1000,
+      config = function()
+        vim.g.gruvbox_material_enable_italic = true
+        vim.cmd.colorscheme('gruvbox-material')
+      end
+    }
+
 	use 'nvim-lua/plenary.nvim' -- dependency for telescope
 	use 'nvim-telescope/telescope.nvim' -- fuzzy finder
 	use {
@@ -19,5 +28,19 @@ return require('packer').startup(function(use)
 			require('keymaps').setup()
 		end
 	}
+	use {
+		"nvim-treesitter/nvim-treesitter", -- syntax highlighting for different languages
+		run = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup {
+				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python", "javascript", "toml", "tsx", "java", "html", "css" },
+				sync_install = false,
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			}
+		end,
+	}
+
 end)
 
