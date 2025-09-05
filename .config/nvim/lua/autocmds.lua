ccf63dc674bff390bf5688ddec9d98bcc2457b47
sudoKush <404kushfound@gmail.com>
2025-09-05 08:00:00 +0530

    chore(nvim): set default tab widths to 2 and enable system clipboard

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,7 +1,10 @@
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
 
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
+
+vim.opt.clipboard:append("unnamedplus") -- using system clipboard as default register
 
 
