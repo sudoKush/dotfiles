d28c0d5e4ed1edd03726f4416b6895ad5999a7bb
sudoKush <404kushfound@gmail.com>
2025-09-09 11:33:17 +0530

    chore(nvim): remove autocmds.lua

--- .config/nvim/lua/autocmds.lua
+++
@@ -1,9 +0,0 @@
-vim.opt.relativenumber = true
-vim.opt.cursorline = true
-
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.smartindent = true
-
-vim.opt.clipboard:append("unnamedplus") -- using system clipboard as default register
-vim.opt.guicursor = "n-v-c-i:block" -- i like block cursor for all them modes
