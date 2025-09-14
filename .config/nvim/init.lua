8a6c6e23318d5937379c27740695dad3df6cd6eb
sudoKush <404kushfound@gmail.com>
2025-09-14 16:56:40 +0530

    chore(nvim): remove the transparent background (for no distractions)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,29 +11,6 @@ vim.opt.clipboard:append("unnamedplus") -- using system clipboard as default reg
 vim.opt.guicursor = "n-v-c-i:block" -- i like block cursor for all them modes
 vim.opt.swapfile = false; -- disables annoying swapfiles 
 
-local transparent_bg = function()
-	vim.cmd [[
-		hi Normal guibg=NONE
-		hi NormalNC guibg=NONE
-		hi SignColumn guibg=NONE
-		hi VertSplit guibg=NONE
-		hi StatusLine guibg=NONE
-		hi LineNr guibg=NONE
-		hi CursorLineNr guibg=NONE
-		hi Pmenu guibg=NONE
-		hi PmenuSel guibg=NONE
-		hi NormalFloat guibg=NONE
-		hi FloatBorder guibg=NONE
-		]]
-end
-
-vim.api.nvim_create_autocmd("ColorScheme", {
-	pattern = "*",
-	callback = transparent_bg
-})
-
-transparent_bg()
-
 require('plugins')
 require('keymaps')
 
