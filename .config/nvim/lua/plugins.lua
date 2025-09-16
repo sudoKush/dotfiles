ae4eb30271d7000ffc462cb5d133cbd042367082
sudoKush <404kushfound@gmail.com>
2025-09-16 10:25:47 +0530

    feat(nvim): add render-markdown plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -110,4 +110,6 @@ return require('packer').startup(function(use)
 	use 'eandrju/cellular-automaton.nvim'  -- a useless plugin but cool plugin
 
 	use 'kdheepak/lazygit.nvim' -- a really useful plugin for terminal git ui
+
+	use 'MeanderingProgrammer/render-markdown.nvim' -- for rendering markdowns in normal mode
 end)
